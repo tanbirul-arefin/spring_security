d7f4a72ab535cd3583467d44ee64296e16b951d1
tanbirul-arefin <mdtanbirularefin67@gmail.com>
2026-09-17 16:01:43 +0600

    1 to 35 minit

--- src/main/java/com/real/springsecurity/Controller/MainController.java
+++ src/main/java/com/real/springsecurity/Controller/MainController.java
@@ -2,17 +2,40 @@ package com.real.springsecurity.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class MainController {
 
+    private boolean isloging;
+
     @GetMapping("/login")
     public String loginpage(){
+
         return "login-page";
     }
 
+
+    @PostMapping("/login")
+    public String dologin(@RequestParam String username, @RequestParam String password, RedirectAttributes redirectAttributes ){
+        if(username.equals("real") && password.equals("123")){
+            isloging = true;
+            return "redirect:/protected";
+        }
+        redirectAttributes.addFlashAttribute("warning","invalid username or password");
+
+        return "redirect:/login";
+    }
+
+
     @GetMapping("/protected")
     public String protectedPage(){
+
+        if(!isloging){
+            return "redirect:/login";
+        }
         return "protected-page";
     }
 }
