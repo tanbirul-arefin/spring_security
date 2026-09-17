6527ff0ac303786b1d4206f79233e31d696725d3
tanbirul-arefin <mdtanbirularefin67@gmail.com>
2026-09-17 17:25:48 +0600

    1 to 58 minit ,again add security and

---
+++ src/main/java/com/real/springsecurity/Security/webSecurityConfig.java
@@ -0,0 +1,11 @@
+package com.real.springsecurity.Security;
+
+import org.springframework.context.annotation.Configuration;
+
+@
+@Configuration
+static class WebSecurityConfigConfiguration {
+    
+}
+public class webSecurityConfig {
+}
