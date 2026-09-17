2046c06f6550282d732a9e22f335fbaa29df41bf
tanbirul-arefin <mdtanbirularefin67@gmail.com>
2026-09-17 17:34:21 +0600

    1 to 104 minit ,again add some new feature

--- src/main/java/com/real/springsecurity/Security/webSecurityConfig.java
+++ src/main/java/com/real/springsecurity/Security/webSecurityConfig.java
@@ -1,11 +1,27 @@
 package com.real.springsecurity.Security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
-@
+
 @Configuration
-static class WebSecurityConfigConfiguration {
-    
-}
+
 public class webSecurityConfig {
+
+    @Bean
+
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(headerConfigure -> headerConfigure.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+
+                .formLogin(formLoginConfigurer -> formLoginConfigurer
+                        .loginPage("/login")
+                ).build();
+    }
+
 }
