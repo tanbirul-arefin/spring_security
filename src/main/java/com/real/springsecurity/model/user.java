39567bc2696cdc9fc9e13fbc58bfa32a1d99f583
tanbirul-arefin <mdtanbirularefin67@gmail.com>
2026-09-18 09:31:49 +0600

    1 to 120 minit@GeneratedValue dile defult vabe row number gulo niye nei and table name diya lage

--- src/main/java/com/real/springsecurity/model/user.java
+++ src/main/java/com/real/springsecurity/model/user.java
@@ -1,10 +1,7 @@
 package com.real.springsecurity.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,11 +12,12 @@ import lombok.ToString;
 @Data
 @ToString
 @Entity
+@Table(name = "user_table")
 public class user {
 
     @Id
-    @GeneratedValue
-    private int id;
+   // @GeneratedValue
+    private Integer id;
     private String username;
     private String password;
 }
